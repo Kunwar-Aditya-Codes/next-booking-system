d62acf4ad9f9f6241c162ac178cc67651f1e5aa2
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-02-22 07:43:10 +0530

    added: modal inputs

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useState } from 'react';
+import ModalInput from './ModalInput';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
 const Modal = ({ isSignIn }: { isSignIn: boolean }) => {
   const [open, setOpen] = useState<boolean>(false);
@@ -23,28 +25,22 @@ const Modal = ({ isSignIn }: { isSignIn: boolean }) => {
 
       {open && (
         <div className='fixed top-0 left-0 w-full h-full bg-black/50 z-50'>
-          <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 bg-slate-900 rounded-2xl space-y-6'>
-            <div className='flex justify-end px-4 py-2'>
-              <button onClick={() => setOpen(false)}>X</button>
-            </div>
-            <div className='flex justify-center '>
-              <h1 className='text-2xl'>Login</h1>
+          <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[18rem] md:w-[30rem] h-[40rem] bg-slate-900 rounded-2xl space-y-6'>
+            <div className='flex justify-end px-4 py-2 '>
+              <button onClick={() => setOpen(false)}>
+                <XMarkIcon className='h-6 w-6 ' />
+              </button>
             </div>
             <div className='flex justify-center'>
-              <form className='flex flex-col'>
-                <input
-                  type='text'
-                  placeholder='Username'
-                  className='border-2 border-slate-800 bg-transparent rounded-md p-2 my-2'
-                />
-                <input
-                  type='password'
-                  placeholder='Password'
-                  className='border-2 border-slate-800 bg-transparent rounded-md p-2 my-2'
-                />
-
-                <button className='bg-slate-800  rounded-md p-2 my-2'>
-                  Login
+              <h1 className='text-2xl'>
+                {renderContent('Sign In', 'Sign Up')}
+              </h1>
+            </div>
+            <div className='flex justify-center '>
+              <form className='flex flex-col w-full px-6'>
+                <ModalInput />
+                <button className='bg-slate-800 shadow-lg rounded-md p-2 my-2'>
+                  {renderContent('Login', 'Create Account')}
                 </button>
               </form>
             </div>
