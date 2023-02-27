52d04f30967584e80fed83c69a81f7e3afce3b28
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-02-27 14:33:30 +0530

    updated: middleware

--- pages/api/user/info.ts
+++ pages/api/user/info.ts
@@ -4,7 +4,19 @@ import prisma from '@/lib/prisma';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
-    
+    const { id } = req.user;
+    const user = await prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        city: true,
+        phone: true,
+      },
+    });
+    return res.status(200).json(user);
   }
 
   return res.status(405).json({ message: 'Method not allowed' });
